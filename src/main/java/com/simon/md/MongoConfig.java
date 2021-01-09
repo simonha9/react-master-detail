b9aeabe31200b90d013aa99e41f0a2dc596b9c2c
Simon Ha <simon9ha@gmail.com>
2021-01-09 16:04:02 -0500

    Configuring Mongo

---
+++ src/main/java/com/simon/md/MongoConfig.java
@@ -0,0 +1,40 @@
+package com.simon.md;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+	public static String dbName = "md-users";
+
+	@Override
+	protected String getDatabaseName() {
+		return dbName;
+	}
+
+	@Override
+	public MongoClient mongoClient() {
+		ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/" + dbName);
+		MongoClientSettings mongoClientSettings = MongoClientSettings.builder().applyConnectionString(connectionString)
+				.build();
+
+		return MongoClients.create(mongoClientSettings);
+	}
+
+	@Override
+	public Collection getMappingBasePackages() {
+		return Collections.singleton("com.simon");
+	}
+
+}
