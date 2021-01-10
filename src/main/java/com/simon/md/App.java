63c27b52a5baa90c72885d4f1322b397432dc099
Simon Ha <simon9ha@gmail.com>
2021-01-10 12:54:27 -0500

    Adding cors mapping for frontend

--- src/main/java/com/simon/md/App.java
+++ src/main/java/com/simon/md/App.java
@@ -1,10 +1,24 @@
 package com.simon.md;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class App {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
