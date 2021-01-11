6a84ed3ddcfdcee48970b7d1f56a7ecdaf8aa663
Simon Ha <simon9ha@gmail.com>
2021-01-11 17:19:53 -0500

    Updating user service impl

--- src/main/java/com/simon/md/service/impl/UserServiceImpl.java
+++ src/main/java/com/simon/md/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.simon.md.service;
+package com.simon.md.service.impl;
 
 import java.util.List;
 
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.simon.md.domain.User;
 import com.simon.md.repository.UserRepository;
+import com.simon.md.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService {
